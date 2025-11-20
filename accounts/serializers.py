496d4aa71038450fc1e52b4139e4e8693f2d5a9f
olumbah1 <adeoyeolumbah@gmail.com>
2025-11-20 14:23:00 +0100

    feat(auth): Added Login, forgot password and reset password serializers

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from django.contrib.auth.password_validation import validate_password
 from django.core.exceptions import ValidationError as DjangoValidationError
+from django.contrib.auth import authenticate
 
 User = get_user_model()
 
@@ -12,9 +13,9 @@ class UserSerializer(serializers.ModelSerializer):
 
 
 class SignUpSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, style={'input_password': 'password'})
+    password = serializers.CharField(write_only=True, style={'input_type': 'password'})
     confirm_password = serializers.CharField(
-        write_only=True, style={'input_password': 'password'})
+        write_only=True, style={'input_type': 'password'})
     
     models = User
     fields = ['first_name', 'last_name', 'email',
@@ -41,3 +42,51 @@ class SignUpSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError({"password": list(exc.messages)})
         
         return attrs
+
+   
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(write_only=True, style={'input_type': 'password'})
+    
+    def validate(self, attrs):
+        email = attrs.get('email')
+        password = attrs.get('password')
+        
+        if email and password:
+            user = authenticate(email=email, password=password)
+            if not user:
+                raise serializers.ValidationError('Invalid email or password')
+            if not user.is_active:
+                raise serializers.ValidationError('User account is disabled')
+            attrs['user'] = user
+            return attrs
+        else:
+            raise serializers.ValidationError('Most include email and password')       
+
+
+class ForgotPasswordSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    
+class ResetPasswordSerializer(serializers.Serializer):
+    uid = serializers.CharField()
+    token = serializers.CharField()
+    new_password = serializers.CharField(write_only=True, style={'input_type': 'password'})
+    confirm_password = serializers.CharField(write_only=True, style={'input_type': 'password'})
+    
+    def validate(self, attrs):
+        pw = attrs.get('password')
+        pw_confirm = attrs.get('password')
+        
+        if not pw or not pw_confirm:
+            raise serializers.ValidationError({"detail": "Both password fields are required."})
+        
+        if pw != pw_confirm:
+            raise serializers.ValidationError({"confirm_password": "password do not match."})
+        
+        try:
+            validate_password(pw)
+        except DjangoValidationError as exc:
+            raise serializers.ValidationError({"new_password": list(exc.messages)})
+        
+        return attrs
+    
