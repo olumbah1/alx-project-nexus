d73d19df8d716c0cceea272c4e7759125526e2f9
olumbah1 <adeoyeolumbah@gmail.com>
2025-11-20 12:13:44 +0100

    feat(auth): Created a custom user that login with email

---
+++ accounts/models.py
@@ -0,0 +1,21 @@
+import uuid
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from .managers import CustomUserManager
+
+
+class CustomUser(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True, null=False, blank=False)
+    username = None
+    
+    
+    USERNAME_FIELD = ['email']
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+    
+    
+    objects = CustomUserManager()
+    def __str__(self):
+        return self.email
